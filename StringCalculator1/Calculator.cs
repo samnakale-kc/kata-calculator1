d3e16c99231d4327b761d8407788fafa545892b1
samnakale-kc <sam.nakale@kaleidocode.co.za>
2025-01-09 10:07:07 +0200

    Initialize project

---
+++ StringCalculator1/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator1
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
+
+    }
+}
