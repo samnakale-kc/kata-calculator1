b607a8536807f3b67cd0fd7f151322afdcf1ebb9
samnakale-kc <sam.nakale@kaleidocode.co.za>
2025-01-09 11:10:11 +0200

    Updated Add method for all tests for requirment 4 to pass

--- StringCalculator1/Calculator.cs
+++ StringCalculator1/Calculator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace StringCalculator1
@@ -15,6 +16,25 @@ namespace StringCalculator1
             if (cleanNumbersInput.Length == 0)
             {
                 return 0;
+            }
+
+            Match match = Regex.Match(cleanNumbersInput, @"\/\/(.+)\n*");
+
+            if (match.Success)
+            {
+                string customDelimeter = match.Groups[1].Value;
+                string firstLine = match.Value;
+                cleanNumbersInput = cleanNumbersInput.Replace(firstLine, String.Empty);
+                cleanNumbersInput = cleanNumbersInput.Replace(customDelimeter, ",");
+            }
+
+            Match matchWithoutFirstLine = Regex.Match(cleanNumbersInput, @"([^\d]+)");
+
+            if (matchWithoutFirstLine.Success)
+            {
+                string customDelimeter = matchWithoutFirstLine.Groups[1].Value;
+                string firstLine = matchWithoutFirstLine.Value;
+                cleanNumbersInput = cleanNumbersInput.Replace(customDelimeter, ",");
             }
 
             char[] stringDelimeters = [',', '\n'];
