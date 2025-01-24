3ecca95a728a8ac649955da2bb688eaec5ea647a
samnakale-kc <sam.nakale@kaleidocode.co.za>
2025-01-24 04:12:15 +0200

    Added tests for requirement 1

---
+++ StringCalculator1/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringCalculator1
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            return 99;
+        }
+    }
+}
---
+++ StringCalculator1Tests/StringCalculator1Tests.cs
@@ -0,0 +1,37 @@
+﻿using StringCalculator1;
+
+namespace StringCalculator1Tests
+{
+    public class StringCalculator1Tests
+    {
+        [Fact]
+        public void GivenEmptyString_WhenAddCalled_ThenReturnZero()
+        {
+            // Arrange
+            var calculator = new Calculator();
+            string numbers = "";
+            const int expectedResult = 0;
+
+            // Act
+            int result = calculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void GivenOneOrTwoNumbers_WhenAddCalled_ThenReturnSum(string numbers, int expectedResult)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            int result = calculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
