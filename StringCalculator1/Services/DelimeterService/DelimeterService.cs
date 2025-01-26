6adfe53b5b9e715e0793a6dfc63a81a2d0dd5ecb
samnakale-kc <sam.nakale@kaleidocode.co.za>
2025-01-26 21:38:49 +0200

    Added the ability to handle multiple delimeters in input string

--- StringCalculator1/Services/DelimeterService/DelimeterService.cs
+++ StringCalculator1/Services/DelimeterService/DelimeterService.cs
@@ -11,20 +11,29 @@ namespace StringCalculator1.Services.DelimeterService
 
         private readonly string _customDelimeterNewLineStartString = @"//";
         private readonly string[] _defaultdelimeters = [",", "\n"];
+        private readonly string _multiDelimeterStartString = "[";
+        private readonly string _multiDelimeterEndString = "]";
+        private readonly string _newLineCharacter = "\n";
 
         public string[] GetNumbersFromDelimetedString(string numbers)
         {
             string[] delimetersToSplitStringBy = _defaultdelimeters;
             bool inputHasFirstLineWithDelimeter = InputHasFirstLineWithDelimeter(numbers);
             bool inputHasCustomDelimeter = InputHasCustomDelimeter(numbers);
+            bool inputHasMultipleDelimeters = InputHasMultipleDelimeters(numbers);
 
             if (!inputHasFirstLineWithDelimeter && inputHasCustomDelimeter)
             {
                 string customDelimeter = GetCustomDelimeterFromInputWithoutFirstLine(numbers);
                 delimetersToSplitStringBy = [customDelimeter];
             }
-
-            if (inputHasFirstLineWithDelimeter)
+            else if (inputHasMultipleDelimeters)
+            {
+                string[] customDelimeter = GetCustomDelimetersFromInputWithMuliDelimeters(numbers);
+                numbers = RemoveFirstLineFromCustomDelimetedInput(numbers);
+                delimetersToSplitStringBy = customDelimeter;
+            }
+            else if (inputHasFirstLineWithDelimeter)
             {
                 string customDelimeter = GetCustomDelimeterFromFirstLine(numbers);
                 numbers = RemoveFirstLineFromCustomDelimetedInput(numbers);
@@ -34,9 +43,41 @@ namespace StringCalculator1.Services.DelimeterService
             return numbers.Split(delimetersToSplitStringBy, StringSplitOptions.RemoveEmptyEntries);
         }
 
+        public bool InputHasMultipleDelimeters(string input)
+        {
+            string startsWith = _customDelimeterNewLineStartString + _multiDelimeterStartString;
+            string endOfDelimeterLine = _multiDelimeterEndString + _newLineCharacter;
+
+            if (input.StartsWith(startsWith) && input.Contains(_multiDelimeterEndString))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public string[] GetCustomDelimetersFromInputWithMuliDelimeters(string input)
+        {
+            string firstLine = input.Split(_newLineCharacter)[0].Replace(_customDelimeterNewLineStartString, string.Empty);
+
+            string[] delimeters = firstLine.Split(_multiDelimeterEndString, StringSplitOptions.RemoveEmptyEntries);
+
+
+            for (int i = 0; i < delimeters.Length; i++)
+            {
+                string delimeter = delimeters[i];
+                string cleanDelimeter = delimeter.Replace(_multiDelimeterStartString, string.Empty).Replace(_multiDelimeterEndString, string.Empty);
+
+                delimeters[i] = cleanDelimeter;
+            }
+
+            return delimeters;
+        }
+
         public string GetCustomDelimeterFromFirstLine(string input)
         {
-            return input.Split('\n')[0].Replace(_customDelimeterNewLineStartString, string.Empty);
+            return input.Split(_newLineCharacter)[0].Replace(_customDelimeterNewLineStartString, string.Empty);
         }
 
         public string GetCustomDelimeterFromInputWithoutFirstLine(string input)
@@ -70,7 +111,7 @@ namespace StringCalculator1.Services.DelimeterService
 
         public bool InputHasFirstLineWithDelimeter(string input)
         {
-            if (input.StartsWith(_customDelimeterNewLineStartString) && input.Contains('\n'))
+            if (input.StartsWith(_customDelimeterNewLineStartString) && input.Contains(_newLineCharacter))
             {
                 return true;
             }
@@ -87,10 +128,10 @@ namespace StringCalculator1.Services.DelimeterService
                 return input;
             }
 
-            var stringSections = input.Split('\n').ToList();
+            var stringSections = input.Split(_newLineCharacter).ToList();
             stringSections.RemoveAt(0); // Removes the first line directly
 
-            return string.Join("\n", stringSections);
+            return string.Join(_newLineCharacter, stringSections);
         }
     }
 }
